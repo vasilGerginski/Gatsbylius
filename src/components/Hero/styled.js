215506ed1cfbe1509727bd64d4d930bbce08b293
imhaage <mahaage@gmail.com>
2020-02-04 17:33:09 +0100

    Reduce <Hero /> height and improve style

--- src/components/Hero/styled.js
+++ src/components/Hero/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { media } from "styled-bootstrap-grid";
 import BackgroundImage from "gatsby-background-image";
 import { spacing, color } from "../../helpers/themeHelpers";
@@ -6,9 +6,12 @@ import { spacing, color } from "../../helpers/themeHelpers";
 export const StyledBackgroundImage = styled(BackgroundImage)`
   position: relative;
   width: 100%;
-  height: 50vh;
-  ${media.md`
-    height: calc(100vh - ${spacing(["lg"])});
+  height: 250px;
+  ${media.sm`
+    height: 300px;
+  `}
+  ${media.lg`
+    height: 550px;
   `}
 `;
 
@@ -22,32 +25,54 @@ export const Overlay = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.65);
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 
-export const PageTitle = styled.h1`
-  position: relative;
-  font-size: 5.5vw;
-  margin-bottom: ${spacing(["xl"])};
-  letter-spacing: 0.1em;
+const cssTitle = css`
+  font-weight: bold;
+  line-height: 1.2;
+  text-transform: uppercase;
   color: ${color("white")};
   text-shadow: ${({ theme }) => theme.boxShadows.textMedium};
 `;
 
-export const ProductsAnchor = styled.button`
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  font-weight: bold;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${color("white")};
-  background: transparent;
-  border: 1px solid ${color("white")};
-  transition: background 0.3s, color 0.3s;
+export const HeroTitle = styled.h1`
+  ${cssTitle}
+  font-size: 7vw;
+  letter-spacing: 0.18em;
+  margin-bottom: ${spacing(["sm"])};
+  ${media.lg`
+    margin-bottom: ${spacing(["md"])};
+    font-size: 5.5vw;
+  `}
+`;
 
-  &:hover {
-    cursor: pointer;
-    color: ${color("black")};
-    background: ${color("white")};
+export const HeroSubTitle = styled.p`
+  ${cssTitle}
+  position: relative;
+  margin-top: 0;
+  font-size: 4.5vw;
+  letter-spacing: 0.15em;
+  ${media.lg`
+    font-size: 3.5vw;
+  `}
+
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+
+    width: 4vw;
+    border: 1px solid ${color("white")};
+  }
+
+  &::before {
+    left: -6vw;
+  }
+
+  &::after {
+    right: -6vw;
   }
 `;
