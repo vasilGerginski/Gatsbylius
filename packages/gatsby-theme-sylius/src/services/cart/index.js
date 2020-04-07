b61bd062750b904e4a1cf7d0fbc4af32ad32b33d
Gaëtan Kueny <gaetan@occitech.fr>
2020-04-07 22:26:39 +0200

    chore: remove gatsby theme & demo & update doc

--- packages/gatsby-theme-sylius/src/services/cart/index.js
+++
@@ -1,5 +0,0 @@
-export { addVariantToCart } from "./addItem";
-export { ensureCartKey } from "./cartKey";
-export { incrementQty, decrementQty } from "./changeItemQty";
-export { dropCart } from "./dropCart";
-export { removeItemFromCart } from "./removeItem";
